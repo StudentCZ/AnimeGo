9f7a7912c2ae56f92beecd300f19c042e82bbb4c
Chao Zhou <Czhou0k@gmail.com>
2022-10-31 12:57:11 -0400

    importing mui components

--- src/AnimeCard.js
+++ src/AnimeCard.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import style from './AnimeCard.module.css';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function AnimeCard(props) {
   return (
