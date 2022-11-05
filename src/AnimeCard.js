3944cf10217b265437cf29b654a16f109234cfc1
Chao Zhou <Czhou0k@gmail.com>
2022-11-04 21:27:05 -0400

    got correct margin and spacing mui mobile

--- src/AnimeCard.js
+++ src/AnimeCard.js
@@ -13,6 +13,7 @@ const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
+  margin: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
