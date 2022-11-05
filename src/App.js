024296a9ad60d164e2b4c8a0aba6f4d63a3c248c
Chao Zhou <Czhou0k@gmail.com>
2022-11-04 21:37:53 -0400

    added progress image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import style from './App.module.css';
 import Search from './Search';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function App() {
   const initalAnime = 'Naruto';
