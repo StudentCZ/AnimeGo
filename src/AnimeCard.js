97f72cc6bdc8bd17d04d3dfd57594a22317badb1
Chao Zhou <Czhou0k@gmail.com>
2022-11-04 20:47:14 -0400

    import boiler plate theme

--- src/AnimeCard.js
+++ src/AnimeCard.js
@@ -9,6 +9,14 @@ import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 function AnimeCard(props) {
   return (
     <article>
